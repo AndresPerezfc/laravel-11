dc0f1e2a2dd1a35579a9b3e9971e6b6dc1200689
andresperezfc <felipe3apc@gmail.com>
2024-08-26 21:19:11 -0500

    Rutas con nombre

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//Rutas con nombre
+
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
